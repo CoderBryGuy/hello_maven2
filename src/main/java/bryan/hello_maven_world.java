64f4d9e2104bb5b8913f7a7eba002cde15880556
Bryan <bryan.dov.bergman@gmail.com>
2020-11-24 21:31:55 +0200

    added logger

--- src/main/java/bryan/hello_maven_world.java
+++ src/main/java/bryan/hello_maven_world.java
@@ -1,7 +1,18 @@
 package bryan;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class hello_maven_world {
+
+    private final static Logger log = LoggerFactory.getLogger(hello_maven_world.class);
+
     public static void main(String[] args) {
-        System.out.println("ur momma");
+//        System.out.println("hi momma");
+
+        log.info("Hello Info");
+        log.info("Hello Debug");
+
     }
 }
